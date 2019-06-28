863596350c48ddf2eb72017a42ef4dfeab73c133
shijing.dz <shijing.dz@founder.com.cn>
2019-06-28 18:31:57 +0800

    init project

---
+++ src/main/java/com/shijing/nopainnogain/NopainnogainApplication.java
@@ -0,0 +1,13 @@
+package com.shijing.nopainnogain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NopainnogainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NopainnogainApplication.class, args);
+    }
+
+}
