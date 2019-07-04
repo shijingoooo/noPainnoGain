5e7ad4ea57dae1f101ed5dcb8407922391d319b4
shijingoooo <shijingoooo@gmail.com>
2019-07-04 22:09:37 +0800

    spring boot打war包在tomcat下访问(兼容jar包直接运行)

--- src/main/java/com/shijing/nopainnogain/NopainnogainApplication.java
+++ src/main/java/com/shijing/nopainnogain/NopainnogainApplication.java
@@ -2,9 +2,15 @@ package com.shijing.nopainnogain;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class NopainnogainApplication {
+public class NopainnogainApplication extends SpringBootServletInitializer {
+
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(NopainnogainApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(NopainnogainApplication.class, args);
