c4162a69bfaeb469038f1310ef2a60cb76fc0db7
shijing.dz <shijing.dz@founder.com.cn>
2019-11-08 11:07:32 +0800

    1.添加reflect测试代码。2.添加线程相关代码

---
+++ src/main/java/com/shijing/nopainnogain/Thread/ObservableThread/EmptyLifecycle.java
@@ -0,0 +1,28 @@
+package com.shijing.nopainnogain.Thread.ObservableThread;
+
+/**
+ * @Auther: shijing
+ * @Date: 19/4/24 14:55
+ * @Description:
+ */
+public class EmptyLifecycle<T> implements TaskLifecycle<T> {
+    @Override
+    public void onStart(Thread thread) {
+
+    }
+
+    @Override
+    public void onRunning(Thread thread) {
+
+    }
+
+    @Override
+    public void onFinish(Thread thread, T result) {
+
+    }
+
+    @Override
+    public void onError(Thread thread, Exception e) {
+
+    }
+}
