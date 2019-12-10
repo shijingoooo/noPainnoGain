d5166a1f1b5afd7249474d420cd8a7ac12ee9130
shijingoooo <shijingoooo@gmail.com>
2019-12-10 23:02:56 +0800

    强引用、软引用

---
+++ src/main/java/com/shijing/nopainnogain/Demo/Reference/StrongReferenceDemo.java
@@ -0,0 +1,14 @@
+package com.shijing.nopainnogain.Demo.Reference;
+
+/**
+ *
+ */
+public class StrongReferenceDemo {
+    public static void main(String[] args) {
+        Object o1 = new Object();
+        Object o2 = o1;
+        o1 = null;
+        System.gc();
+        System.out.println(o2);
+    }
+}
