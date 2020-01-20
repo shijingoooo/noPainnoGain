ed337e8ca640b70a790925aa5ceee6846ff038d9
shijing.dz <shijing.dz@founder.com.cn>
2020-01-20 17:14:35 +0800

    springBoot EnableScheduling

---
+++ src/main/java/com/shijing/nopainnogain/Spring/service/ScheduledDemo.java
@@ -0,0 +1,16 @@
+package com.shijing.nopainnogain.Spring.service;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScheduledDemo {
+
+    /**
+     * 0 0 0 ? * 1,5
+     */
+    @Scheduled(cron = "* * * * * ?")
+    public void doJob() {
+        System.out.println(System.currentTimeMillis());
+    }
+}
