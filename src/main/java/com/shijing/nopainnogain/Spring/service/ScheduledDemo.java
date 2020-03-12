5fb00813084124fb5955785eded558acf8286e88
shijingoooo <shijingoooo@gmail.com>
2020-03-12 11:08:55 +0800

    注释定时任务

--- src/main/java/com/shijing/nopainnogain/Spring/service/ScheduledDemo.java
+++ src/main/java/com/shijing/nopainnogain/Spring/service/ScheduledDemo.java
@@ -9,8 +9,8 @@ public class ScheduledDemo {
     /**
      * 0 0 0 ? * 1,5
      */
-    @Scheduled(cron = "* * * * * ?")
+    /*@Scheduled(cron = "* * * * * ?")
     public void doJob() {
         System.out.println(System.currentTimeMillis());
-    }
+    }*/
 }
