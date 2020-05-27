543a4dcb4427170e73357bf5a613a56636757436
shijingoooo <shijingoooo@gmail.com>
2020-05-27 23:23:56 +0800

    数组两个索引之和

---
+++ src/main/java/com/shijing/nopainnogain/algorithm/tree/PathSum.java
@@ -0,0 +1,69 @@
+package com.shijing.nopainnogain.algorithm.tree;
+
+import java.util.Stack;
+
+/**
+ * 给定一个二叉树，它的每个结点都存放着一个整数值。
+ *
+ * 找出路径和等于给定数值的路径总数。
+ *
+ * 路径不需要从根节点开始，也不需要在叶子节点结束，但是路径方向必须是向下的（只能从父节点到子节点）。
+ *
+ * 二叉树不超过1000个节点，且节点数值范围是 [-1000000,1000000] 的整数。
+ *
+ * 示例：
+ *
+ * root = [10,5,-3,3,2,null,11,3,-2,null,1], sum = 8
+ *
+ *       10
+ *      /  \
+ *     5   -3
+ *    / \    \
+ *   3   2   11
+ *  / \   \
+ * 3  -2   1
+ *
+ * 返回 3。和等于 8 的路径有:
+ *
+ * 1.  5 -> 3
+ * 2.  5 -> 2 -> 1
+ * 3.  -3 -> 11
+ *
+ * @description:
+ * @author: shijing
+ * @create: 2020-05-26 23:54
+ **/
+public class PathSum {
+
+    public static void main(String[] args) {
+
+        TreeNode root = new TreeNode(null, null, 1);
+
+        PathSum pathSum = new PathSum();
+
+        int sum = pathSum.pathSum(root, 10);
+
+        System.out.println("sum=" + sum);
+
+    }
+
+    public int pathSum(TreeNode root, int sum) {
+        if(root == null){
+            return 0;
+        }
+        int result = countPath(root,sum);
+        int a = pathSum(root.left,sum);
+        int b = pathSum(root.right,sum);
+        return result+a+b;
+
+    }
+    public int countPath(TreeNode root,int sum){
+        if(root == null){
+            return 0;
+        }
+        sum = sum - root.value;
+        int result = sum == 0 ? 1:0;
+        return result + countPath(root.left,sum) + countPath(root.right,sum);
+    }
+
+}
