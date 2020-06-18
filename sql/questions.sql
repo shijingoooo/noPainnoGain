fb8f5bcaf57c9630b01e765d9e6b9d909441f01b
shijingoooo <shijingoooo@gmail.com>
2020-06-18 22:10:38 +0800

    DATEDIFF 使用

--- sql/questions.sql
+++ sql/questions.sql
@@ -298,3 +298,26 @@ AND (e.Salary, e.DepartmentId) IN
   FROM Employee
   GROUP BY DepartmentId
 )
+
+-- 给定一个 Weather 表，编写一个 SQL 查询，来查找与之前（昨天的）日期相比温度更高的所有日期的 Id。
+--
+-- +---------+------------------+------------------+
+-- | Id(INT) | RecordDate(DATE) | Temperature(INT) |
+-- +---------+------------------+------------------+
+-- |       1 |       2015-01-01 |               10 |
+-- |       2 |       2015-01-02 |               25 |
+-- |       3 |       2015-01-03 |               20 |
+-- |       4 |       2015-01-04 |               30 |
+-- +---------+------------------+------------------+
+-- 例如，根据上述给定的 Weather 表格，返回如下 Id:
+--
+-- +----+
+-- | Id |
+-- +----+
+-- |  2 |
+-- |  4 |
+-- +----+
+SELECT w1.Id
+FROM Weather w1 JOIN Weather w2
+ON DATEDIFF(w1.RecordDate, w2.RecordDate) = 1
+AND w1.Temperature > w2.Temperature
