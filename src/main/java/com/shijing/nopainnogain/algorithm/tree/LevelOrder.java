3b0e0c46c9757613821871c1aecdba0297faae06
shijingoooo <shijingoooo@gmail.com>
2020-08-03 23:17:52 +0800

    树的层次遍历

---
+++ src/main/java/com/shijing/nopainnogain/algorithm/tree/LevelOrder.java
@@ -0,0 +1,67 @@
+package com.shijing.nopainnogain.algorithm.tree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 给你一个二叉树，请你返回其按 层序遍历 得到的节点值。 （即逐层地，从左到右访问所有节点）。
+ * <p>
+ *  
+ * <p>
+ * 示例：
+ * 二叉树：[3,9,20,null,null,15,7],
+ * <p>
+ * 3
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
+ * 返回其层次遍历结果：
+ * <p>
+ * [
+ * [3],
+ * [9,20],
+ * [15,7]
+ * ]
+ *
+ * @description:
+ * @author: shijing
+ * @create: 2020-08-03 23:15
+ **/
+public class LevelOrder {
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+
+        List<List<Integer>> res = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            int count = queue.size();
+            List<Integer> list = new ArrayList<>();
+            while (count > 0) {
+                TreeNode node = queue.poll();
+                list.add(node.val);
+
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+
+                count--;
+            }
+            res.add(list);
+        }
+
+        return res;
+    }
+
+}
